be62dac837c377c46f3309d80a82d9f82ebfabb2
Brennan <merrett.b@gmail.com>
2022-07-20 15:43:27 -0400

    added validations

--- models/Comment.js
+++ models/Comment.js
@@ -9,10 +9,12 @@ const ReplySchema = new Schema(
       default: () => new Types.ObjectId()
     },
     replyBody: {
-      type: String
+      type: String,
+      required: true
     },
     writtenBy: {
-      type: String
+      type: String,
+      required: true,
     },
     createdAt: {
       type: Date,
@@ -30,10 +32,12 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
+      required: true
     },
     commentBody: {
-      type: String
+      type: String,
+      required: true
     },
     createdAt: {
       type: Date,
@@ -51,6 +55,7 @@ const CommentSchema = new Schema(
     id: false
   }
 );
+
 
 CommentSchema.virtual('replyCount').get(function() {
   return this.replies.length;
